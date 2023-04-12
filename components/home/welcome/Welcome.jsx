096b0e4e93d1bb93662b972d1efe1cb77198de17
obomheire <obomheire@gmail.com>
2023-04-12 13:08:19 +0100

    added welcome.jsx need to fix error alert DMRegular is  not a system font

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-
+import React, { useState} from 'react'
+import { View, Text, TextInput, TouchableOpacity, Image, FlatList} from 'react-native'
+import { useRouter } from 'expo-router'
 import styles from './welcome.style'
+import { COLOR, COLORS, icons, images, SIZES } from "../../../constants";
 
 const Welcome = () => {
+
+  const router = useRouter();
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Zack</Text>
+         <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+      </View>
+      
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+        <TextInput />
+    </View>
+    </View>
     </View>
   )
 }
 
-export default Welcome
+export default Welcome  
